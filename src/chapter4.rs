5eb0b4b0c9b307067d44b740d82c4eb8f99012e0
Lynton Dicks <lynton@pvs.global>
2025-09-29 21:46:25 +0200

    Fix unused variable warning in chapter 4 solution

--- src/chapter4.rs
+++ src/chapter4.rs
@@ -3,7 +3,7 @@ pub fn main() {
     open_store("Downtown", 5);    
 }
 
-fn open_store(neighborhood: &str, store_count: u32) -> () {
+fn open_store(neighborhood: &str, _store_count: u32) -> () {
     println!("Opening store in {neighborhood} neighborhood.");
     let result = square(13);
     println!("The square of 13 is: {result}");
