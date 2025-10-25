c223241607f6716051ee7f35767d63e55d14347e
Lynton Dicks <lynton@pvs.global>
2025-10-25 12:42:31 +0200

    Create Workspace and move existing app to solutions_app folder. Add solutions_app as member of workspace.

---
+++ solutions-app/src/main.rs
@@ -0,0 +1,28 @@
+use solutions_app::chapter2;
+use solutions_app::chapter3;
+use solutions_app::chapter4;
+use solutions_app::chapter5;
+use solutions_app::chapter6;
+use solutions_app::chapter7;
+use solutions_app::chapter8;
+use solutions_app::chapter9;
+use solutions_app::chapter10;
+use solutions_app::chapter11;
+use solutions_app::chapter12;
+use solutions_app::chapter13;
+
+fn main() {
+    println!("Learn to code with Rust!");
+    chapter2::main();
+    chapter3::main();
+    chapter4::main();
+    chapter5::main();
+    chapter6::main();
+    chapter7::main();
+    chapter8::main();
+    chapter9::main();
+    chapter10::main();
+    chapter11::main();
+    chapter12::main();
+    chapter13::main();
+}
