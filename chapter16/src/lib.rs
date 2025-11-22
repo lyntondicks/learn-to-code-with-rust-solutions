5670d43ed8b8115ea99016a4f50217d3f364f739
Lynton Dicks <lynton@pvs.global>
2025-11-22 18:59:35 +0100

    Extract Project 16 project solution to submodule.

--- chapter16/src/lib.rs
+++ chapter16/src/lib.rs
@@ -1,35 +1,13 @@
 mod hash_maps;
 mod hash_sets;
-
-use std::collections::HashMap;
+mod project_solution;
 
 use hash_maps::hash_maps;
 use hash_sets::hash_sets;
+use project_solution::chapter_16_project_solution;
 
 pub fn chapter16() {
     hash_maps();
     hash_sets();
-
     chapter_16_project_solution();
-}
-
-fn chapter_16_project_solution() {
-    let mut sauces_to_meals = HashMap::from([
-        ("Ketchup", vec!["French Fries", "Burgers", "Hot Dogs"]),
-        ("Mayonnaise", vec!["Sandwiches", "Burgers", "Coleslaw"]),
-    ]);
-
-    sauces_to_meals.insert("Mustard", vec!["Hot Dog", "Burgers", "Pretzels"]);
-    println!("{:#?}", sauces_to_meals.remove("Mayonnaise").unwrap());
-
-    let mustard_meals = sauces_to_meals.get("Mustard");
-    match mustard_meals {
-        Some(meals) => println!("Mustard goes well with: {:?}", meals),
-        None => println!("No meals found for Mustard"),
-    }
-
-    sauces_to_meals
-        .entry("Soy Sauce")
-        .or_insert(vec!["Sushi", "Dumplings"]);
-    println!("{:#?}", sauces_to_meals);
 }
---
+++ chapter16/src/project_solution.rs
@@ -0,0 +1,22 @@
+use std::collections::HashMap;
+
+pub fn chapter_16_project_solution() {
+    let mut sauces_to_meals = HashMap::from([
+        ("Ketchup", vec!["French Fries", "Burgers", "Hot Dogs"]),
+        ("Mayonnaise", vec!["Sandwiches", "Burgers", "Coleslaw"]),
+    ]);
+
+    sauces_to_meals.insert("Mustard", vec!["Hot Dog", "Burgers", "Pretzels"]);
+    println!("{:#?}", sauces_to_meals.remove("Mayonnaise").unwrap());
+
+    let mustard_meals = sauces_to_meals.get("Mustard");
+    match mustard_meals {
+        Some(meals) => println!("Mustard goes well with: {:?}", meals),
+        None => println!("No meals found for Mustard"),
+    }
+
+    sauces_to_meals
+        .entry("Soy Sauce")
+        .or_insert(vec!["Sushi", "Dumplings"]);
+    println!("{:#?}", sauces_to_meals);
+}
