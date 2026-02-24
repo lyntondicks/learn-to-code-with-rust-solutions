fa4f3d9ad506d9f1577047931c90559cc9630fd5
Lynton Dicks <lynton@pvs.global>
2026-02-24 10:11:14 +0100

    Add Eq trait module to chapter 18.

---
+++ chapter18/src/eq_trait.rs
@@ -0,0 +1,38 @@
+#[derive(Debug, PartialEq, Eq)]
+struct Flight {
+    origin: String,
+    destination: String,
+    time: String,
+}
+
+impl Flight {
+    fn new(origin: &str, destination: &str, time: &str) -> Self {
+        Self {
+            origin: origin.to_string(),
+            destination: destination.to_string(),
+            time: time.to_string(),
+        }
+    }
+}
+
+pub fn eq_trait() {
+    println!("Implementing the Eq trait");
+
+    let a = Flight::new("New York", "Cape Town", "08:00");
+    let b = Flight::new("New York", "Cape Town", "08:00");
+    let c = Flight::new("New York", "Cape Town", "08:00");
+
+    // Rules for Eq trait
+    println!("{}", a == a); // Reflexive a == a
+    println!("{}", a == b);
+    println!("{}", b == a); // symmetric a==b implies b==a
+    println!("{}", a == c); // transitive, a == b, b == c, implies a == c
+
+    // why, not all types naturally support Eq, e.g. float because of NaN
+    let division = 0.0 / 0.0;
+    println!("{}", division);
+
+    let value = 3.14;
+    println!("{}", value == value); // true
+    println!("{}", division == division); // false... NaN can not be compared to NaN
+}
--- chapter18/src/lib.rs
+++ chapter18/src/lib.rs
@@ -2,6 +2,7 @@ mod associated_constants;
 mod clone_trait;
 mod copy_trait;
 mod display_trait;
+mod eq_trait;
 mod partial_eq;
 mod trait_def_and_impl;
 mod traits_must_be_in_scope;
@@ -10,6 +11,7 @@ use associated_constants::associated_constants;
 use clone_trait::clone_trait;
 use copy_trait::copy_trait;
 use display_trait::display_trait;
+use eq_trait::eq_trait;
 use partial_eq::partial_eq;
 use trait_def_and_impl::trait_def_and_impl;
 use traits_must_be_in_scope::traits_must_be_in_scope;
@@ -23,4 +25,5 @@ pub fn chapter18() {
     clone_trait();
     copy_trait();
     partial_eq();
+    eq_trait();
 }
