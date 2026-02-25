e8ee47a264790ed1094240fd3814fccd2130f35e
Lynton Dicks <lynton@pvs.global>
2026-02-25 19:52:52 +0100

    Add Partial Ord Trait example to Chapter 18.

---
+++ chapter18/src/partial_ord_trait.rs
@@ -0,0 +1,42 @@
+use std::cmp::Ordering;
+
+#[allow(dead_code)]
+struct Job {
+    salary: u32,
+    commute_time: u32,
+}
+
+impl PartialEq for Job {
+    fn eq(&self, other: &Self) -> bool {
+        self.salary == other.salary
+    }
+}
+
+impl Eq for Job {}
+
+impl PartialOrd for Job {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.salary.partial_cmp(&other.salary)
+    }
+}
+
+pub fn partial_ord_trait() {
+    println!("PartialOrd trait");
+
+    let long_commute_job = Job {
+        salary: 10_000,
+        commute_time: 2,
+    };
+
+    let short_commute_job = Job {
+        salary: 75_000,
+        commute_time: 1,
+    };
+
+    println!("{}", long_commute_job > short_commute_job);
+    println!("{}", long_commute_job < short_commute_job);
+    println!("{}", long_commute_job == short_commute_job);
+    println!("{}", long_commute_job >= short_commute_job);
+    println!("{}", long_commute_job <= short_commute_job);
+    println!("{}", long_commute_job != short_commute_job);
+}
