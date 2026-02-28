14e1196b5df7013e23dd173752e9d88734d591d6
Lynton Dicks <lynton@pvs.global>
2026-02-28 11:30:43 +0100

    Add Associated types example, using the Add trait, to the chapter 18 project.

---
+++ chapter18/src/associated_types.rs
@@ -0,0 +1,32 @@
+use std::ops::Add;
+
+#[derive(Debug)]
+struct Lunch {
+    cost: f64,
+}
+
+impl Add for Lunch {
+    type Output = Lunch;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Lunch {
+            cost: self.cost + rhs.cost,
+        }
+    }
+}
+
+fn add_two_numbers<T: Add<Output = T>>(a: T, b: T) -> T {
+    a + b
+}
+
+pub fn associated_types() {
+    println!("Associated Types");
+
+    let lunch1 = Lunch { cost: 10.0 };
+    let lunch2 = Lunch { cost: 15.0 };
+    let total_cost = lunch1 + lunch2;
+    println!("Total lunch cost: ${:.2}", total_cost.cost);
+
+    println!("{}", add_two_numbers(3.14, 2.3));
+    println!("{}", add_two_numbers(2, 3));
+}
