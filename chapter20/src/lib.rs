3543e671b4ddbb7c8b2efabedb1b26dd4076daf0
Lynton Dicks <lynton@pvs.global>
2026-03-16 21:12:30 +0100

    Add project solution for chapter 20.

--- chapter20/src/lib.rs
+++ chapter20/src/lib.rs
@@ -1,11 +1,22 @@
 mod fn_mut_trait;
 mod fn_once_trait;
+mod project_solution;
 
 use fn_mut_trait::fn_mut_trait;
 use fn_once_trait::fn_once_trait;
+use project_solution::project_solution;
 
 pub fn chapter20() {
     println!("Chapter 20: Closures");
     fn_once_trait();
     fn_mut_trait();
+    fn_argument();
+    project_solution();
+}
+
+fn fn_argument() {
+    println!("Demonstrate passing a function argument to a fn trait param");
+    let option: Option<Vec<String>> = None;
+    let collection = option.unwrap_or_else(Vec::new); // Pass in Vec::new function as closure
+    println!("{collection:?}");
 }
