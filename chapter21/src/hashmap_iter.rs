5b139de837e8d174d78ef0079cec65315ec74a9f
Lynton Dicks <lynton@pvs.global>
2026-03-29 14:57:22 +0200

    Add hashmap iteration demonstration to chapter 21.

---
+++ chapter21/src/hashmap_iter.rs
@@ -0,0 +1,16 @@
+use std::collections::HashMap;
+
+pub fn hashmap_iter() {
+    println!("Chapter 21: HashMap Iteration");
+
+    let mut todos = HashMap::new();
+    todos.insert("Pick up groceries", false);
+    todos.insert("Study Rust", true);
+    todos.insert("Sleep", false);
+
+    for (_, completion_status) in &mut todos {
+        *completion_status = true;
+    }
+
+    println!("{todos:?}");
+}
