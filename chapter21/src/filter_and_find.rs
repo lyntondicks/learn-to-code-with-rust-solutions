402fa21df86c5a38d8ac232ad013319f1af10938
Lynton Dicks <lynton@pvs.global>
2026-04-07 17:14:53 +0200

    Add filter and find with complex types to chapter 21

--- chapter21/src/filter_and_find.rs
+++ chapter21/src/filter_and_find.rs
@@ -1,6 +1,7 @@
 pub fn filter_and_find() {
     println!("Chapter 21: Filter and Find");
     filter_and_find_simple_types();
+    filter_and_find_complex_types();
 }
 
 pub fn filter_and_find_simple_types() {
@@ -22,4 +23,54 @@ pub fn filter_and_find_simple_types() {
 
     let last_even = numbers.into_iter().rfind(|x| x % 2 == 0);
     println!("Last even number: {:?}", last_even);
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum ChannelType {
+    Comedy,
+    News,
+    ProgrammingTutorials,
+}
+
+#[derive(Debug)]
+struct TvChannel {
+    name: String,
+    channel_type: ChannelType,
+}
+
+pub fn filter_and_find_complex_types() {
+    let channels = [
+        TvChannel {
+            name: String::from("CBS"),
+            channel_type: ChannelType::Comedy,
+        },
+        TvChannel {
+            name: String::from("RustLive"),
+            channel_type: ChannelType::ProgrammingTutorials,
+        },
+        TvChannel {
+            name: String::from("NBC"),
+            channel_type: ChannelType::News,
+        },
+        TvChannel {
+            name: String::from("RustTV"),
+            channel_type: ChannelType::ProgrammingTutorials,
+        },
+    ];
+
+    let good_channels: Vec<&str> = channels
+        .iter()
+        .filter(|channel| channel.channel_type == ChannelType::ProgrammingTutorials)
+        .map(|channel| channel.name.as_str())
+        .collect();
+    println!("Good channels: {:?}", good_channels);
+
+    let good_channel = channels
+        .iter()
+        .find(|channel| channel.channel_type == ChannelType::ProgrammingTutorials);
+
+    match good_channel {
+        Some(channel) => println!("First good channel: {:?}", channel),
+        None => println!("No good channels found"),
+    }
 }
